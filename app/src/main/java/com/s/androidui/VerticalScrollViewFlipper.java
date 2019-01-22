0ee5c6aef1e3c616125ff075e50ebff05ece21c8
amylovesong <amylovesong.sun@gmail.com>
2019-01-22 17:01:24 +0800

    Create project

---
+++ app/src/main/java/com/s/androidui/VerticalScrollViewFlipper.java
@@ -0,0 +1,6 @@
+package com.s.androidui;
+
+import android.widget.ViewFlipper;
+
+public class VerticalScrollViewFlipper extends ViewFlipper {
+}
---
+++ app/src/main/java/com/s/androidui/sample/AdapterViewFlipperDemo.java
@@ -0,0 +1,13 @@
+package com.s.androidui.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdapterViewFlipperDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adapter_view_flipper_demo);
+    }
+}
