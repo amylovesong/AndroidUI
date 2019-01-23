f7cb33cb9d7c1b2eb11239fd0fed9ad3e7a1ba56
amylovesong <amylovesong.sun@gmail.com>
2019-01-23 15:03:18 +0800

    Optimize VerticalScrollViewFlipper

--- app/src/main/java/com/s/androidui/VerticalScrollViewFlipper.java
+++ app/src/main/java/com/s/androidui/VerticalScrollViewFlipper.java
@@ -1,11 +1,16 @@
 package com.s.androidui;
 
 import android.content.Context;
+import android.database.DataSetObserver;
+import android.support.annotation.NonNull;
 import android.util.AttributeSet;
 import android.widget.Adapter;
 import android.widget.ViewFlipper;
 
 public class VerticalScrollViewFlipper extends ViewFlipper {
+
+    private Adapter mAdapter;
+    private DataSetObserver mDataSetObserver;
 
     public VerticalScrollViewFlipper(Context context) {
         super(context);
@@ -17,18 +22,44 @@ public class VerticalScrollViewFlipper extends ViewFlipper {
         init(context);
     }
 
+    public void setAdapter(@NonNull Adapter adapter) {
+        if (this.mAdapter == adapter) {
+            return;
+        }
+        if (this.mAdapter != null) {
+            this.mAdapter.unregisterDataSetObserver(mDataSetObserver);
+        }
+        this.mAdapter = adapter;
+        this.mAdapter.registerDataSetObserver(mDataSetObserver);
+    }
+
     private void init(Context context) {
-        setInAnimation(context, R.anim.anim_in);
-        setOutAnimation(context, R.anim.anim_out);
+        initDataSetObserver();
+
+        setInAnimation(context, R.anim.anim_in_translate_y);
+        setOutAnimation(context, R.anim.anim_out_translate_y);
 
         setFlipInterval(3000);
         setAnimateFirstView(false);
     }
 
-    public void setAdapter(Adapter adapter) {
+    private void initDataSetObserver() {
+        if (this.mDataSetObserver == null) {
+            this.mDataSetObserver = new DataSetObserver() {
+                @Override
+                public void onChanged() {
+                    super.onChanged();
+                    onDataChanged(mAdapter);
+                }
+            };
+        }
+    }
+
+    private void onDataChanged(Adapter adapter) {
         if (isFlipping()) {
             stopFlipping();
         }
+        removeAllViews();
         for (int i = 0; i < adapter.getCount(); i++) {
             addView(adapter.getView(i, null, this));
         }
